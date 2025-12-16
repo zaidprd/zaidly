2ed538763342e9f9a2311b57093abcb10b6895a4
zaidprd <ahmadzaid.tamiang@gmail.com>
2025-12-16 13:46:37 +0700

    feat: Final setup before Cloudflare deploy (Sitemap, CMS config)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
+// astro.config.mjs
+
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind"; 
+import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap'; // <-- Import Sitemap
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()], 
+  // WAJIB: Tentukan domain utama situs Anda untuk Sitemap
+  site: 'https://www.zaidly.com', 
+  
+  integrations: [
+    tailwind(),
+    sitemap(), // <-- Tambahkan Sitemap
+  ],
 });
