3b2f522657b999e89c448708a06b5c8cd8fc53ba
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-01-11 09:38:28 +0700

    Update output mode and remove Cloudflare adapter
    Changed output mode from 'server' to 'static' and removed Cloudflare adapter configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://zaidly.com',
-  
-  // Tetap SSR sesuai request abang
-  output: 'server', 
-  
-  adapter: cloudflare({
-    mode: 'directory',
-    runtime: { mode: 'complete' } 
-  }),
+  output: 'static',
 
-  // FIX UTAMA: Matikan image service bawaan agar tidak crash 1101
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop'
